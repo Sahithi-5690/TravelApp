89419123273e2b59f5e98864c8ec5d865ea87e9a
Leela Krishna Andhe <142929100+LeelaKrishna97@users.noreply.github.com>
2024-11-21 16:29:13 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/androidproject/Activity/DetailActivity.java
@@ -0,0 +1,55 @@
+package com.example.androidproject.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.example.androidproject.Domain.ItemDomain;
+import com.example.androidproject.R;
+import com.example.androidproject.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends BaseActivity {
+    ActivityDetailBinding binding;
+    private ItemDomain object;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        setVariable();
+
+    }
+
+    private void setVariable() {
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$" + object.getPrice());
+        binding.backBtn.setOnClickListener(v -> finish());
+        //binding.descriptionTxt.setText(object.getDescription());
+        binding.addressTxt.setText(object.getAddress());
+        binding.ratingTxt.setText(object.getScore() + " Rating");
+        binding.ratingBar.setRating((float) object.getScore());
+
+        Glide.with(DetailActivity.this)
+                .load(object.getPic())
+                .into(binding.pic);
+
+        binding.addToCartBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(DetailActivity.this, TicketActivity.class);
+            intent.putExtra("object", object);
+            startActivity(intent);
+        });
+    }
+
+    private void getIntentExtra() {
+        object = (ItemDomain) getIntent().getSerializableExtra("object");
+    }
+}
